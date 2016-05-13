e064afd8e34d550a9354d9397047367fd6f04499
Jesse Rosalia <jesse.rosalia@gatech.edu>
2016-05-13 13:05:46 -0400

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-datastore "0.2.0-SNAPSHOT"
+(defproject clj-datastore "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
