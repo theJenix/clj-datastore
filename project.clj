7c9558a1c8dc50e20e38d44d14d5e7e4815bb224
Jesse Rosalia <jesse@rosalia.me>
2018-01-08 17:00:48 -0800

    incremented revision number, due to changes with local-storage

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-datastore "0.3.3-SNAPSHOT"
+(defproject clj-datastore "0.3.4-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
